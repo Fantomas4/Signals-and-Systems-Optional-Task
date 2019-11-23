bbb741137c92d04122a3f5e3a421fdd5954c3078
Fantomas4 <stefanokf@auth.gr>
2019-11-23 13:52:24 +0200

    Worked on the algorithm for task A.1 (WIP)

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-
-
-
-
-
-
-
 if __name__ == '__main__':
+    # Get user's input for N and check whether it is greater than 10
     a_vector_len = int(input("Enter the random vector length N (N>10): "))
     while a_vector_len <= 10:
         a_vector_len = int(input("Wrong input. N must be greater than 10. Try again: "))
@@ -27,3 +21,21 @@ if __name__ == '__main__':
     print("DIAG: random_vector: ", a_vector)
 
     b_vector = [1/5, 1/5, 1/5, 1/5, 1/5]
+
+    # y_vector holds the result of the convolution
+    # initialize the y_vector with 0s
+    y_vector = [0] * (len(a_vector) + len(b_vector) - 1)
+    
+    print("DIAG: y_vector:", y_vector)
+
+    # i holds the index of the first element of a_vector that belongs
+    # to the sliding window's current instance
+    i = 0
+
+    # j holds the index of the last element of a_vector that belongs
+    # to the sliding window's current instance
+    j = len(b_vector) - 1
+
+    # n holds the index of the result vector where
+    # we are currently doing calculations
+    n = 0

42067dd930333fb39188f45d54425504f6eee711
Fantomas4 <stefanokf@auth.gr>
2019-12-15 13:17:45 +0200

    Made minor changes to files. Cleaned up and finished the implementation of taskA1.py

--- my_convolve.py
+++ my_convolve.py
@@ -3,8 +3,6 @@ def MyConvolve(a_vector, b_vector):
     # initialize the y_vector with 0s
     y_vector = [0] * (len(a_vector) + len(b_vector) - 1)
 
-    # print("DIAG: y_vector:", y_vector)
-
     # i holds the index of the first element of a_vector that belongs
     # to the sliding window's current instance
     i = 0
@@ -17,53 +15,39 @@ def MyConvolve(a_vector, b_vector):
     # we are currently doing calculations
     n = 0
 
-
     # At first, the sliding window has not fully covered
     # the indexes of vector A
-    import tqdm
-    pbar = tqdm.tqdm(total=len(b_vector))
-
+    # (The sliding window is entering A vector's frame)
     while j < len(b_vector) - 1:
-        # print("DIAG: Loop1")
         b_index = 0
         for k in range(i, j + 1):
             y_vector[n] += a_vector[k] * b_vector[b_index]
             b_index += 1
         j += 1
         n += 1
-        pbar.update(1)
-    pbar.close()
 
     # The sliding window has now fully covered the first
-    # 5 indexes of vector A
-    pbar = tqdm.tqdm(total=len(a_vector)-j-1)
+    # 5 (5 is b_vector's size) indexes of vector A
+    # (the sliding windows has entered A vector's frame)
     while j < len(a_vector):
-        print("DIAG: Loop2")
         b_index = 0
         for k in range(i, j + 1):
             y_vector[n] += a_vector[k] * b_vector[b_index]
             b_index += 1
-        print("y_vector is: " + str(y_vector))
-        # print("DIAG: For n = " + str(n) + " y_vector is: " + str(y_vector[n]))
         i += 1
         j += 1
         n += 1
-        pbar.update(1)
-    pbar.close()
 
-    # Finally, the sliding window has to fully stop covering all
-    # of A vector's indexes
-    pbar = tqdm.tqdm(total=j-i-1)
+    # Finally, the sliding window has to stop covering
+    # A vector's indexes
+    # (The slide window is leaving A vector's frame)
     while i < j:
-        print("DIAG: Loop3")
         b_index = 0
         for k in range(i, j):
             y_vector[n] += a_vector[k] * b_vector[b_index]
             b_index += 1
         i += 1
         n += 1
-        pbar.update(1)
-    pbar.close()
 
     return y_vector
 
--- taskA1.py
+++ taskA1.py
@@ -1,35 +1,22 @@
-import test2
+import numpy
+
+import my_convolve
 
 if __name__ == '__main__':
     # Get user's input for N and check whether it is greater than 10
-    # a_vector_len = int(input("Enter the random vector length N (N>10): "))
-    # while a_vector_len <= 10:
-    #     a_vector_len = int(input("Wrong input. N must be greater than 10. Try again: "))
-    #
-    # print("DIAG: vector_len: ", a_vector_len)
-    a_vector_len = 13
+    a_vector_len = int(input("=> Enter the random vector length N (N>10): "))
+    while a_vector_len <= 10:
+        a_vector_len = int(input("\n=> Wrong input. N must be greater than 10. Try again: "))
 
-    # # generate random floating point values
-    # from numpy.random import seed
-    # from numpy.random import rand
-    #
-    # # seed random number generator
-    # seed(1)
-    # # generate random numbers between 0-1
-    # a_vector = rand(a_vector_len)
+    # Use numpy's random.uniform to generate random floating numbers from within the range [-100.0, 100.0)
+    a_vector = numpy.random.uniform(low=-100.0, high=100.0, size=a_vector_len)
 
-    # import random
-    # a_vector = random.sample(range(1, 100), a_vector_len)
-
-    # TEST ONLY!
-    a_vector = [1, 4, 5, 6, 7, 8, 9, 10, 22, 56, 2, 3, 11]
-
-    print("DIAG: a_vector: ", a_vector)
+    print("\n=> The random vector generated with the requested length is: ", a_vector)
 
     b_vector = [1/5, 1/5, 1/5, 1/5, 1/5]
 
-    print("DIAG: b_vector: ", b_vector)
+    print("\n=> The second vector used for the convolution is: ", b_vector)
 
-    print(test2.MyConvolve(a_vector, b_vector))
+    print("\n=> The result vector of the convolution is: ", my_convolve.MyConvolve(a_vector, b_vector))
 
 
